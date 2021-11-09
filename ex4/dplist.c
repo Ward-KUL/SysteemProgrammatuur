7029d44295cf6f2efeeb464b754d6a237ad121a7
Ward Smets <ward.smets1@student.kuleuven.be>
2021-11-09 13:56:15 +0100

    oefening 4 free bijna gedaan, alleen nog problemen met inserten van twee elementen op dezelfde index. Dan wordt niet alles tegoei gefreed

---
+++ ex4/dplist.c
@@ -0,0 +1,234 @@
+/**
+ * \author Ward Smets
+ */
+
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+#include "dplist.h"
+
+/*
+ * definition of error codes
+ * */
+#define DPLIST_NO_ERROR 0
+#define DPLIST_MEMORY_ERROR 1 // error due to mem alloc failure
+#define DPLIST_INVALID_ERROR 2 //error due to a list operation applied on a NULL list 
+
+#ifdef DEBUG
+#define DEBUG_PRINTF(...) 									                                        \
+        do {											                                            \
+            fprintf(stderr,"\nIn %s - function %s at line %d: ", __FILE__, __func__, __LINE__);	    \
+            fprintf(stderr,__VA_ARGS__);								                            \
+            fflush(stderr);                                                                         \
+                } while(0)
+#else
+#define DEBUG_PRINTF(...) (void)0
+#endif
+
+
+#define DPLIST_ERR_HANDLER(condition, err_code)                         \
+    do {                                                                \
+            if ((condition)) DEBUG_PRINTF(#condition " failed\n");      \
+            assert(!(condition));                                       \
+        } while(0)
+
+
+/*
+ * The real definition of struct list / struct node
+ */
+
+struct dplist_node {
+    dplist_node_t *prev, *next;
+    void *element;
+};
+
+struct dplist {
+    dplist_node_t *head;
+
+    void *(*element_copy)(void *src_element);
+
+    void (*element_free)(void **element);
+
+    int (*element_compare)(void *x, void *y);
+};
+
+dplist_node_t* create_new_node(void* element,bool insert_copy);
+
+
+dplist_t *dpl_create(// callback functions
+        void *(*element_copy)(void *src_element),
+        void (*element_free)(void **element),
+        int (*element_compare)(void *x, void *y)
+) {
+    dplist_t *list;
+    list = malloc(sizeof(struct dplist));
+    DPLIST_ERR_HANDLER(list == NULL, DPLIST_MEMORY_ERROR);
+    list->head = NULL;
+    list->element_copy = element_copy;
+    list->element_free = element_free;
+    list->element_compare = element_compare;
+    return list;
+}
+
+void dpl_free(dplist_t **list, bool free_element) {
+
+    if(*list != NULL){
+        dplist_node_t* node = (*list)->head;
+        dplist_node_t* previous = NULL;
+        while(node != NULL){
+            if(free_element == true) (*list)->element_free(&(node->element));//call element free
+            previous = node;
+            node = node->next;
+            free(previous);
+        }
+        free(*list);
+        node = NULL;
+        *list = NULL;
+
+    }
+
+}
+
+dplist_t *dpl_insert_at_index(dplist_t *list, void *element, int index, bool insert_copy) {
+
+    printf("nothing is done yet with bool insert_copy\n");
+
+    if(list == NULL) return NULL;
+    if(list->head == NULL) {
+        //the element given will aways be the firste element in the list
+        list->head = create_new_node(element,insert_copy);
+    }
+    else{
+            dplist_node_t* node = create_new_node(element,insert_copy);
+            if(index<= 0){
+                node = create_new_node(element,insert_copy);
+                node->next = list->head;
+                list->head->prev = node;
+                list->head = node;
+            }
+            else{
+                int size = dpl_size(list);
+                if(index >(size -1)){
+                    //add a new element at the back of the list
+                    node->prev = dpl_get_reference_at_index(list,size - 1);
+                    node->prev->next = node;
+                }
+                else{
+                    node->prev = dpl_get_reference_at_index(list,index-1);
+                    node->next = node->prev->next;
+                    node->prev->next = node;
+                    node->next->prev = node;
+                }
+            }
+            return list;
+
+    }
+
+}
+
+dplist_node_t* create_new_node(void* element,bool insert_copy){
+    //printf("%d: created an element of size %ld\ns",__LINE__,sizeof(dplist_node_t));
+    dplist_node_t* newNode = malloc(sizeof(dplist_node_t));
+    newNode->element = element;
+    newNode->next = NULL;
+    newNode->prev = NULL;
+    return newNode;
+}
+
+dplist_t *dpl_remove_at_index(dplist_t *list, int index, bool free_element) {
+    dplist_node_t* node;
+    node = list->head;
+    if(list == NULL) return NULL;
+    unsigned int size = dpl_size(list);
+    if(index<=0) {
+        list->head = node->next;
+        list->element_free(node->element);
+        free(node);
+        return list;
+    }
+    else if(index>(size-1)) index = size -1;
+    //loop till at the correct node
+    int i = 0;
+    while(i <(index)){
+        DPLIST_ERR_HANDLER(node==NULL,DPLIST_MEMORY_ERROR);
+        node = node->next;
+        i++;    
+    }
+    //arived at the correct index -1
+    //make node point to the next one
+    node->prev->next = node->next;
+    list->element_free(node->element);
+    free(node);
+    return list;
+    
+
+}
+
+int dpl_size(dplist_t *list) {
+
+    if(list == NULL) return -1;
+    dplist_node_t* node = list->head;
+    int size = 0;
+    while(node != NULL){
+        node = node->next;
+        size ++;
+    }
+    return size;
+
+}
+
+void *dpl_get_element_at_index(dplist_t *list, int index) {
+
+    if(list == NULL) return NULL;
+    if(index<=0) return list->head->element;
+    int size = dpl_size(list);
+    if(index >= size - 1){
+        index = size - 1;
+    }
+    dplist_node_t* node = list->head;
+    int counter = 0;
+    while(counter<index){
+        node = node->next;
+        counter ++;
+    }
+    return node->element;
+
+}
+
+int dpl_get_index_of_element(dplist_t *list, void *element) {
+
+    if(list == NULL) return -1;
+    dplist_node_t* node = list->head;
+    //dplist_node_t* previousNode;
+    int index = 0;
+    while((node->element) != (element)){
+        if(node == NULL) return -1; //element not found
+        node = node->next;
+        index++;
+    }
+    return index;
+}
+
+dplist_node_t *dpl_get_reference_at_index(dplist_t *list, int index) {
+
+    int count;
+    dplist_node_t *dummy;
+    if (list->head == NULL) return NULL;
+    for (dummy = list->head, count = 0; dummy->next != NULL; dummy = dummy->next, count++) {
+        if (count >= index) return dummy;
+    }//TODO: add your code here
+    return dummy;
+
+void *dpl_get_element_at_reference(dplist_t *list, dplist_node_t *reference) {
+
+    if(list == NULL) return NULL;
+    dplist_node_t* node = list->head;
+    while(node!=NULL){
+        if(node == reference) return node->element;
+    }
+    return NULL;
+
+}
+}
+
+
---
+++ ex4/dplist_test.c
@@ -0,0 +1,149 @@
+/**
+ * \author Ward Smets
+ */
+
+#define _GNU_SOURCE
+
+#include "dplist.h"
+#include <check.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <assert.h>
+
+
+
+typedef struct {
+    int id;
+    char* name;
+} my_element_t;
+
+void* element_copy(void * element);
+void element_free(void ** element);
+int element_compare(void * x, void * y);
+
+void * element_copy(void * element) {
+    my_element_t* copy = malloc(sizeof (my_element_t));
+    char* new_name;
+    asprintf(&new_name,"%s",((my_element_t*)element)->name);
+    assert(copy != NULL);
+    copy->id = ((my_element_t*)element)->id;
+    copy->name = new_name;
+    return (void *) copy;
+}
+
+void element_free(void ** element) {
+    //printf("This should be the element: %c\n",*(((my_element_t*)*element))->name);
+    free((((my_element_t*)*element))->name);
+    free(*element);
+    *element = NULL;
+}
+
+int element_compare(void * x, void * y) {
+    return ((((my_element_t*)x)->id < ((my_element_t*)y)->id) ? -1 : (((my_element_t*)x)->id == ((my_element_t*)y)->id) ? 0 : 1);
+}
+
+my_element_t* make_element(int id,char name_c){
+    my_element_t* element = malloc(sizeof(my_element_t));
+    element->id = 1;
+    char* name = malloc(sizeof(char));
+    *name = name_c; 
+    element->name = name;
+    return element;
+}
+
+void setup(void) {
+    // Implement pre-test setup
+}
+
+void teardown(void) {
+    // Implement post-test teardown
+}
+START_TEST(test_ListFree)
+    {
+        dplist_t* list = NULL;
+        dpl_free(&list,false);
+        ck_assert_msg(list == NULL,"Failure: expected null");
+        list = NULL;
+        dpl_free(&list,true);
+        ck_assert_msg(list == NULL,"Failure: expected null");
+
+        list = dpl_create(element_copy,element_free,element_compare);
+        dpl_free(&list,false);
+        ck_assert_msg(list == NULL,"Failure: expected null");
+        list = dpl_create(element_copy,element_free,element_compare);
+        dpl_free(&list,true);
+        ck_assert_msg(list == NULL,"Failure: expected null");
+
+        my_element_t* e1 = make_element(4,'a');
+        element_free((void**)&e1);
+
+        list = dpl_create(element_copy,element_free,element_compare);
+        my_element_t* element = make_element(1,'a');
+        dpl_insert_at_index(list,element,0,false);
+        dpl_free(&list,false);
+        element_free((void**) &element);
+        list = dpl_create(element_copy,element_free,element_compare);
+        element = make_element(2,'b');
+        dpl_insert_at_index(list,element,0,true);
+        dpl_free(&list,true); 
+        ck_assert_msg(list == NULL,"Failure: expected null");
+
+        list = dpl_create(element_copy,element_free,element_compare);
+        my_element_t* e2 = make_element(4,'c');
+        my_element_t* e3 = make_element(5,'d');
+        dpl_insert_at_index(list,e2,0,false);
+        dpl_insert_at_index(list,e3,1,false);
+        dpl_free(&list,false);
+        element_free((void**)&e2);
+        element_free((void**)&e3);
+        ck_assert_msg(list == NULL,"Failure: expected null");
+
+        list = dpl_create(element_copy,element_free,element_compare);
+        my_element_t* e6 = make_element(4,'c');
+        my_element_t* e7 = make_element(5,'d');
+        dpl_insert_at_index(list,e6,0,false);
+        dpl_insert_at_index(list,e7,1,false);
+        dpl_free(&list,true);
+
+        list = dpl_create(element_copy,element_free,element_compare);
+        my_element_t* e8 = make_element(4,'c');
+        my_element_t* e9 = make_element(5,'d');
+        dpl_insert_at_index(list,e8,0,false);
+        dpl_insert_at_index(list,e9,0,false);
+        dpl_free(&list,true);
+
+    }
+END_TEST
+
+START_TEST(test_add_and_get){
+
+   
+    
+
+
+}
+END_TEST
+
+//START_TEST(test_nameOfYourTest)
+//  Add other testcases here...
+//END_TEST
+
+int main(void) {
+    Suite *s1 = suite_create("LIST_EX3");
+    TCase *tc1_1 = tcase_create("Core");
+    SRunner *sr = srunner_create(s1);
+    int nf;
+
+    suite_add_tcase(s1, tc1_1);
+    tcase_add_checked_fixture(tc1_1, setup, teardown);
+    tcase_add_test(tc1_1, test_ListFree);
+    tcase_add_test(tc1_1,test_add_and_get);
+    // Add other tests here...
+
+    srunner_run_all(sr, CK_VERBOSE);
+
+    nf = srunner_ntests_failed(sr);
+    srunner_free(sr);
+
+    return nf == 0 ? EXIT_SUCCESS : EXIT_FAILURE;
+}
