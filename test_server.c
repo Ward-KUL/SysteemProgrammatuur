521dd48ee240d47eebd0f49e0aef5c1c321ab171
Ward Smets <ward.smets1@student.kuleuven.be>
2021-12-06 16:35:26 +0100

    going to poll instead of our own free wheeling idea's

--- test_server.c
+++ test_server.c
@@ -10,26 +10,43 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include "lib/dplist.h"
+#include <poll.h>
 
 #define PORT 5678
 #define MAX_CONN 3  // state the max. number of connections the server will handle before exiting
-#define TIMEOUT 60000
+#define TIMEOUT 2000
 
 
+typedef struct{
+    time_t last_responds;
+    tcpsock_t* client;
+}tcpconn_t;
 
 /**
  * Implements a sequential test server (only one connection at the same time)
  */
-void start_connection_manager();
+void start_connection_manager(dplist_t* list);
 void new_connection_started(tcpsock_t* server,tcpsock_t* client,dplist_t* list);
+void start_timeout_listener(dplist_t* list);
 
 void free_client(void** element){
+    printf("FREE client not yet iplemented\n");
     //free((tcpsock_t*)*element);
 }
 
 
 int main(void) {
-    start_connection_manager();
+    dplist_t* list = dpl_create(NULL,free_client,NULL);
+    pid_t childpid;
+    childpid = fork();
+    if(childpid == 0){
+        //timeout checker
+        start_timeout_listener(list);
+    }
+    else{
+        //incoming connection listener
+        start_connection_manager(list);
+    }
     return 0;
 }
 
@@ -41,15 +58,25 @@ void close_all_connections(dplist_t* list){
     }
 }
 
-void start_connection_manager(){
+void start_timeout_listener(dplist_t* list){
+    while(true){
+        printf("Will check for possible timeouts, amount of connections is %d\n",dpl_size(list));
+        for(int i = 0;i<dpl_size(list);i++){
+            tcpconn_t* conn = dpl_get_element_at_index(list,i);
+            time_t time = time;
+            if((conn->last_responds - time)>TIMEOUT){
+                printf("Connection timed out i try to close\n");
+                tcp_close(&(conn->client));
+                dpl_remove_at_index(list,i,true);
+            }
+        }
+        sleep(1);
+    }
+}
+
+void start_connection_manager(dplist_t* list){
     tcpsock_t *server, *client;
     pid_t childpid;
-    // mypid = getpid();
-
-    // int* conn_counter = malloc(sizeof(int));
-    // *conn_counter = 0;
-    dplist_t* list = dpl_create(NULL,free_client,NULL);
-
 
     printf("Test server is started\n");
     if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
@@ -61,8 +88,11 @@ void start_connection_manager(){
             new_connection_started(server,client,list);
         }
         //else, the parent just keeps listening
-        dpl_insert_at_index(list,&client,99,false);
-        // printf("Currently we have %d TCP_CONNECTIONS open\n",dpl_size(list));
+        tcpconn_t* conn = malloc(sizeof(tcpconn_t));
+        conn->client = client;
+        (conn->last_responds) = (time_t)time;
+        dpl_insert_at_index(list,conn,99,false);
+        printf("Currently we have %d TCP_CONNECTIONS open\n",dpl_size(list));
     
     } while (1);
     if (tcp_close(&server) != TCP_NO_ERROR) exit(EXIT_FAILURE);
@@ -72,7 +102,6 @@ void start_connection_manager(){
 }
 
 void new_connection_started(tcpsock_t* server,tcpsock_t* client,dplist_t* list){
-    time_t last_respondse = time;
     printf("Incoming client connection\n");
     // dpl_insert_at_index(list,client,99,false);
     // printf("Currently we have %p %d TCP_CONNECTIONS open\n",conn_counter,*conn_counter);
@@ -101,7 +130,6 @@ void new_connection_started(tcpsock_t* server,tcpsock_t* client,dplist_t* list){
     else
         printf("Error occured on connection to peer\n");
     // *conn_counter = *conn_counter -1;
-    dpl_remove_element(list,client,false);
     tcp_close(&client);
     exit(0);
 }
