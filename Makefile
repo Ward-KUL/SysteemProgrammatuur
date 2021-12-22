4e3783d0ce12d1794b603521699075c24bbcbae7
Ward Smets <ward.smets1@student.kuleuven.be>
2021-12-22 17:18:01 +0100

    We hebben een werkende poll

--- Makefile
+++ Makefile
@@ -1,8 +1,20 @@
 # Create your own targets that compile the application
 test:
-	gcc -g -o test_server -lpthread test_server.c lib/tcpsock.c
+	gcc -g -o test_server -lpthread test_server.c lib/tcpsock.c lib/dplist.c
 	gcc -g -o client -lpthread sensor_node.c lib/tcpsock.c
 
+server:
+	make test
+	./test_server
+test_orig:
+	gcc -g -o test_server -lpthread test_server_orig.c lib/tcpsock.c lib/dplist.c
+	gcc -g -o client -lpthread sensor_node.c lib/tcpsock.c
+server_o:
+	make test_orig
+	./test_server
+
+client:
+	./client 1 1 127.0.0.1 5678
 
 # the files for ex2 will be ziped and are then ready to
 # be submitted to labtools.groept.be
--- test_server.c
+++ test_server.c
@@ -7,10 +7,24 @@
 #include <inttypes.h>
 #include "config.h"
 #include "lib/tcpsock.h"
+#include "lib/dplist.h"
+#include <poll.h>
+// #include <stropts.h>
+
 
 #define PORT 5678
 #define MAX_CONN 3  // state the max. number of connections the server will handle before exiting
+#define TIMEOUT -1
 
+void free_tcp(void** tcp){
+    free(*tcp);
+}
+void* copy_tcp(void* tcp){
+    printf("TRIED TO USE A FUNCTION WHICH IS NOT IMPLEMENTED\n\n\n\n");
+    // int* copy  = malloc(sizeof(int));
+    // *copy = *((int*)(tcp));
+    // return copy;
+}
 
 /**
  * Implements a sequential test server (only one connection at the same time)
@@ -21,10 +35,24 @@ int main(void) {
     sensor_data_t data;
     int bytes, result;
     int conn_counter = 0;
+    dplist_t* tcp_list = dpl_create(copy_tcp,free_tcp,NULL); //NEEDS TO BE FREED STILL    
 
     printf("Test server is started\n");
     if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+    int* sd = malloc(sizeof(int)); //socket of server
+    tcp_get_sd(server,sd);
+    dpl_insert_at_index(tcp_list,sd,99,false);
     do {
+        //poll for connections
+        struct pollfd fds[2];
+        for(int i = 0;i<2;i++){
+            // add new fds to the fds to be polled
+            printf("%d\n",*((int*)dpl_get_element_at_index(tcp_list,i)));
+            fds[i].fd = *((int*)dpl_get_element_at_index(tcp_list,i));
+            fds[i].events = POLLIN;
+        }
+        int ret = poll(fds,2,TIMEOUT);
+        printf("got past the poll\n");
         if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) exit(EXIT_FAILURE);
         printf("Incoming client connection\n");
         conn_counter++;
