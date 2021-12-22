f983ba34441aea5f0a501bf4b1e7c72400269328
Ward Smets <ward.smets1@student.kuleuven.be>
2021-12-22 17:43:04 +0100

    We hebben een werkende poll :)

--- Makefile
+++ Makefile
@@ -1,18 +1,20 @@
 # Create your own targets that compile the application
 test:
-	gcc -g -o test_server -lpthread test_server.c lib/tcpsock.c lib/dplist.c -Wall
-	gcc -g -o client -lpthread sensor_node.c lib/tcpsock.c lib/dplist.c -Wall
+	gcc -g -o test_server -lpthread test_server.c lib/tcpsock.c lib/dplist.c
+	gcc -g -o client -lpthread sensor_node.c lib/tcpsock.c
+
 server:
 	make test
 	./test_server
+test_orig:
+	gcc -g -o test_server -lpthread test_server_orig.c lib/tcpsock.c lib/dplist.c
+	gcc -g -o client -lpthread sensor_node.c lib/tcpsock.c
+server_o:
+	make test_orig
+	./test_server
+
 client:
 	./client 1 1 127.0.0.1 5678
-servergdb:
-	make test
-	export CK_FORK=no;gdb -tui ./test_server
-
-
-
 
 # the files for ex2 will be ziped and are then ready to
 # be submitted to labtools.groept.be
--- test_server.c
+++ test_server.c
@@ -7,131 +7,79 @@
 #include <inttypes.h>
 #include "config.h"
 #include "lib/tcpsock.h"
-#include <sys/types.h>
-#include <unistd.h>
 #include "lib/dplist.h"
 #include <poll.h>
+// #include <stropts.h>
+
 
 #define PORT 5678
 #define MAX_CONN 3  // state the max. number of connections the server will handle before exiting
-#define TIMEOUT 2000
+#define TIMEOUT -1
 
-
-typedef struct{
-    time_t last_responds;
-    tcpsock_t* client;
-}tcpconn_t;
+void free_tcp(void** tcp){
+    free(*tcp);
+}
+void* copy_tcp(void* tcp){
+    printf("TRIED TO USE A FUNCTION WHICH IS NOT IMPLEMENTED\n\n\n\n");
+    // int* copy  = malloc(sizeof(int));
+    // *copy = *((int*)(tcp));
+    // return copy;
+}
 
 /**
  * Implements a sequential test server (only one connection at the same time)
  */
-void start_connection_manager(dplist_t* list);
-void new_connection_started(tcpsock_t* server,tcpsock_t* client,dplist_t* list);
-void start_timeout_listener(dplist_t* list);
-
-void free_client(void** element){
-    printf("FREE client not yet iplemented\n");
-    //free((tcpsock_t*)*element);
-}
-
 
 int main(void) {
-    dplist_t* list = dpl_create(NULL,free_client,NULL);
-    pid_t childpid;
-    childpid = fork();
-    if(childpid == 0){
-        //timeout checker
-        start_timeout_listener(list);
-    }
-    else{
-        //incoming connection listener
-        start_connection_manager(list);
-    }
-    return 0;
-}
-
-void close_all_connections(dplist_t* list){
-    for(int i = 0;i<dpl_size(list);i++){
-        tcpsock_t* client = dpl_get_element_at_index(list,i);
-        int result = tcp_close(&client);
-        if(result != TCP_NO_ERROR) printf("Failed to close connection\n");
-    }
-}
-
-void start_timeout_listener(dplist_t* list){
-    while(true){
-        printf("Will check for possible timeouts, amount of connections is %d\n",dpl_size(list));
-        for(int i = 0;i<dpl_size(list);i++){
-            tcpconn_t* conn = dpl_get_element_at_index(list,i);
-            time_t time = time;
-            if((conn->last_responds - time)>TIMEOUT){
-                printf("Connection timed out i try to close\n");
-                tcp_close(&(conn->client));
-                dpl_remove_at_index(list,i,true);
-            }
-        }
-        sleep(1);
-    }
-}
-
-void start_connection_manager(dplist_t* list){
     tcpsock_t *server, *client;
-    pid_t childpid;
+    sensor_data_t data;
+    int bytes, result;
+    int conn_counter = 0;
+    dplist_t* tcp_list = dpl_create(copy_tcp,free_tcp,NULL); //NEEDS TO BE FREED STILL    
 
     printf("Test server is started\n");
     if (tcp_passive_open(&server, PORT) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+    int* sd = malloc(sizeof(int)); //socket of server
+    tcp_get_sd(server,sd);
+    dpl_insert_at_index(tcp_list,sd,99,false);
     do {
-        if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) exit(EXIT_FAILURE);
-        childpid = fork();
-        if(childpid == 0){
-            //child process
-            new_connection_started(server,client,list);
+        //poll for connections
+        struct pollfd fds[2];
+        for(int i = 0;i<2;i++){
+            // add new fds to the fds to be polled
+            printf("%d\n",*((int*)dpl_get_element_at_index(tcp_list,i)));
+            fds[i].fd = *((int*)dpl_get_element_at_index(tcp_list,i));
+            fds[i].events = POLLIN;
         }
-        //else, the parent just keeps listening
-        tcpconn_t* conn = malloc(sizeof(tcpconn_t));
-        conn->client = client;
-        (conn->last_responds) = (time_t)time;
-        dpl_insert_at_index(list,conn,99,false);
-        printf("Currently we have %d TCP_CONNECTIONS open\n",dpl_size(list));
-    
-    } while (1);
+        int ret = poll(fds,2,TIMEOUT);
+        printf("got past the poll\n");
+        if (tcp_wait_for_connection(server, &client) != TCP_NO_ERROR) exit(EXIT_FAILURE);
+        printf("Incoming client connection\n");
+        conn_counter++;
+        do {
+            // read sensor ID
+            bytes = sizeof(data.id);
+            result = tcp_receive(client, (void *) &data.id, &bytes);
+            // read temperature
+            bytes = sizeof(data.value);
+            result = tcp_receive(client, (void *) &data.value, &bytes);
+            // read timestamp
+            bytes = sizeof(data.ts);
+            result = tcp_receive(client, (void *) &data.ts, &bytes);
+            if ((result == TCP_NO_ERROR) && bytes) {
+                printf("sensor id = %" PRIu16 " - temperature = %g - timestamp = %ld\n", data.id, data.value,
+                       (long int) data.ts);
+            }
+        } while (result == TCP_NO_ERROR);
+        if (result == TCP_CONNECTION_CLOSED)
+            printf("Peer has closed connection\n");
+        else
+            printf("Error occured on connection to peer\n");
+        tcp_close(&client);
+    } while (conn_counter < MAX_CONN);
     if (tcp_close(&server) != TCP_NO_ERROR) exit(EXIT_FAILURE);
     printf("Test server is shutting down\n");
-    // close_all_connections(list);
-    dpl_free(&list,false);
-}
-
-void new_connection_started(tcpsock_t* server,tcpsock_t* client,dplist_t* list){
-    printf("Incoming client connection\n");
-    // dpl_insert_at_index(list,client,99,false);
-    // printf("Currently we have %p %d TCP_CONNECTIONS open\n",conn_counter,*conn_counter);
-    // *conn_counter = *conn_counter +1;
-    // printf("Currently we have %p %d TCP_CONNECTIONS open\n",conn_counter,*conn_counter);
-
-    sensor_data_t data;
-    int bytes, result;
-    do {
-        // read sensor ID
-        bytes = sizeof(data.id);
-        result = tcp_receive(client, (void *) &data.id, &bytes);
-        // read temperature
-        bytes = sizeof(data.value);
-        result = tcp_receive(client, (void *) &data.value, &bytes);
-        // read timestamp
-        bytes = sizeof(data.ts);
-        result = tcp_receive(client, (void *) &data.ts, &bytes);
-        if ((result == TCP_NO_ERROR) && bytes) {
-            printf("sensor id = %" PRIu16 " - temperature = %g - timestamp = %ld\n", data.id, data.value,
-                (long int) data.ts);
-        }
-    } while (result == TCP_NO_ERROR);
-    if (result == TCP_CONNECTION_CLOSED)
-        printf("Peer has closed connection\n");
-    else
-        printf("Error occured on connection to peer\n");
-    // *conn_counter = *conn_counter -1;
-    tcp_close(&client);
-    exit(0);
+    return 0;
 }
 
 
